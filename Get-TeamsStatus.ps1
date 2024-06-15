3b170087ea62629717f3396c6749ff6c86bb36b4
Rob <robert.mclellan+github@gmail.com>
2024-06-15 10:11:55 -0700

    Update Get-TeamsStatus.ps1
    Updated logic on getting status from the log file.

--- Get-TeamsStatus.ps1
+++ Get-TeamsStatus.ps1
@@ -2,7 +2,6 @@
 .NOTES
     Name: Get-TeamsStatus.ps1
     Author: Danny de Vries
-    New Teams Compatibility Hack: @latetedemelon
     Requires: PowerShell v2 or higher
     Version History: https://github.com/EBOOZ/TeamsStatus/commits/main
 .SYNOPSIS
@@ -78,30 +77,98 @@ DO {
         continue
     }
 
-    try {
-        $logContent = Get-Content -Path $latestLogFile -Tail 1000 | Select-Object -Last 1000
-        $reversedContent = $logContent[$logContent.Count..0]
-        Write-Host "Successfully read the log file content."
-    } catch {
-        Write-Error "Failed to read log file content: $_"
-        Start-Sleep 1
-        continue
+    # Define the string patterns to filter
+    $statuspatterns = @(
+        "Received Action: UserPresenceAction:",
+        "CloudStateChanged: New Cloud State Event:",
+        "BroadcastGlobalState: New Global State Event:",
+        "Received Action: UserPresenceAction:",
+        ": Received Action: UserPresenceAction:"
+    )
+
+    # Get the last 1000 lines from the log file
+    $logContent = Get-Content -Path $latestLogFile -Tail 1000
+
+    # Filter for the specific patterns
+    $filteredLines = $logContent | Where-Object {
+        foreach ($statuspattern in $statuspatterns) {
+            if ($_ -match $statuspattern) {
+                return $true
+            }
+        }
+        return $false
+    }
+    
+    # Select the most recent (last) line
+    if ($null -eq $filteredLines) {
+        Write-Host "No Availability Updates, skipping this iteration."
+    } else {
+        $recentLine = $filteredLines[-1]
     }
 
-    $TeamsStatus = $reversedContent | Select-String -Pattern `
-      'SetTaskbarIconOverlay overlay description',`
-      'Received Action: UserPresenceAction:',`
-      'CloudStateChanged: New Cloud State Event:',`
-      'BroadcastGlobalState: New Global State Event:',`
-      'Received Action: UserPresenceAction:'
-    Write-Host "TeamsStatus entries found: $($TeamsStatus.Count)"
+    # Extract the timestamp (first 32 characters) and availability status
+    if ($recentLine) {
+        $timestamp = $recentLine.Substring(0, 32).Trim()
 
-    # Get Teams Logfile and last app update daemon status
-    $TeamsActivity = $reversedContent | Select-String -Pattern `
-      'EmitWebClientStateChangeEvent',`
-      'SaveHighestWebClientState',`
-      'SaveLastChangedWebClientState'
-    Write-Host "TeamsActivity entries found: $($TeamsActivity.Count)"
+        # Extract the availability status using a regex pattern
+        if ($recentLine -match "availability: (\w+)(.+)$") {
+            $availabilityStatus = $matches[1]
+        } else {
+            $availabilityStatus = "Unknown"
+        }
+
+        # Output the results
+        Write-Host "Timestamp: $timestamp"
+        Write-Host "Availability Status:$availabilityStatus Status:$Status Current Status:$CurrentStatus $recentLine"
+    } else {
+        Write-Host "No matching lines found."
+    }
+
+        # Define the string patterns to filter
+    $activepatterns = @(
+        "EmitWebClientStateChangeEvent",
+        "SaveHighestWebClientState",
+        "SaveLastChangedWebClientState"
+    )
+
+    # Get the last 1000 lines from the log file
+    $logContent = Get-Content -Path $latestLogFile -Tail 1000
+
+    # Filter for the specific patterns
+    $filteredLines = $logContent | Where-Object {
+        foreach ($activepattern in $activepatterns) {
+            if ($_ -match $activepattern) {
+                return $true
+            }
+        }
+        return $false
+    }
+
+    # Select the most recent (last) line
+    if ($null -eq $filteredLines) {
+        Write-Host "No Activity Updates, skipping this iteration."
+    } else {
+        $recentLine = $filteredLines[-1]
+    }
+        
+
+    # Extract the timestamp (first 32 characters) and availability status
+    if ($recentLine) {
+        $timestamp = $recentLine.Substring(0, 32).Trim()
+
+        # Extract the availability status using a regex pattern
+        if ($recentLine -match "state=(\w+)") {
+            $activityStatus = $matches[1]
+        } else {
+            $activityStatus = "Unknown"
+        }
+
+        # Output the results
+        Write-Host "Timestamp: $timestamp"
+        Write-Host "Activity Status: $activityStatus"
+    } else {
+        Write-Host "No matching lines found."
+    }
 
     # Get Teams application process
     $TeamsProcess = Get-Process -Name ms-teams -ErrorAction SilentlyContinue
@@ -109,46 +176,49 @@ DO {
     # Check if Teams is running and start monitoring the log if it is
     If ($null -ne $TeamsProcess) {
         Write-Host "Microsoft Teams process is running."
-        If($TeamsStatus -eq $null){ 
+
+        If($availabilityStatus -eq $null){ 
             Write-Host "No TeamsStatus entries found."
-        } ElseIf ($TeamsStatus -like "*availability: $lgAvailable*") {
+        } ElseIf (($availabilityStatus | Out-String) -like "lgAvailable") {
             $Status = $lgAvailable
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgBusy*") {
+        } ElseIf ($availabilityStatus -like "*$lgBusy*") {
             $Status = $lgBusy
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgAway*") {
+        } ElseIf ($availabilityStatus -like "*$lgAway*") {
             $Status = $lgAway
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgBeRightBack*") {
+        } ElseIf ($availabilityStatus -like "*$lgBeRightBack*") {
             $Status = $lgBeRightBack
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgDoNotDisturb*") {
+        } ElseIf ($availabilityStatus -like "*$lgDoNotDisturb*") {
             $Status = $lgDoNotDisturb
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgFocusing*") {
+        } ElseIf ($availabilityStatus -like "*$lgFocusing*") {
             $Status = $lgFocusing
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgPresenting*") {
+        } ElseIf ($availabilityStatus -like "*$lgPresenting*") {
             $Status = $lgPresenting
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgInAMeeting*") {
+        } ElseIf ($availabilityStatus -like "*$lgInAMeeting*") {
             $Status = $lgInAMeeting
             Write-Host "Status set to: $Status"
-        } ElseIf ($TeamsStatus -like "*availability: $lgOffline*") {
+        } ElseIf ($availabilityStatus -like "*$lgOffline*") {
             $Status = $lgOffline
             Write-Host "Status set to: $Status"
+        } Else {
+            Write-Host "No Match"
         }
 
         If($TeamsActivity -eq $null){ 
             Write-Host "No TeamsActivity entries found."
-        } ElseIf ($TeamsActivity -notlike "*state=VeryActive*") {
-            $Activity = $lgNotInACall
-            $ActivityIcon = $iconNotInACall
-            Write-Host "Activity set to: $Activity"
-        } ElseIf ($TeamsActivity -like "*state=VeryActive*") {
+        } ElseIf ($activityStatus -like "*VeryActive*") {
             $Activity = $lgInACall
             $ActivityIcon = $iconInACall
+            Write-Host "Activity set to: $Activity"
+        } Else {
+            $Activity = $lgNotInACall
+            $ActivityIcon = $iconNotInACall
             Write-Host "Activity set to: $Activity"
         }
     } Else {
@@ -198,5 +268,5 @@ DO {
         }
     }
 
-    Start-Sleep 1
+    Start-Sleep $refreshDelay
 } Until ($Enable -eq 0)
